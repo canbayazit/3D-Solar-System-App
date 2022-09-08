f3385666e2da2233d4819dace3a7cd252cec26db
Can <rovahelsing@gmail.com>
2022-09-08 03:57:38 +0300

    folder structure is creating

---
+++ src/Components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from './style.module.scss';
+const Header = () => {
+  return (
+    <div className={style.container}>
+        <div className={style.world}>            
+        </div>
+        <div className={style.shadow}>            
+        </div>
+    </div>
+  )
+}
+
+export default Header;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../Components/Header';
+import style from './style.module.scss';
+const Home = () => {
+  return (
+    <div className={style.container}>
+        <Header/>
+    </div>
+  )
+}
+
+export default Home;
