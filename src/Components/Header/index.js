e23cf22f1798efb39e1ffa7a46a9ce6e771092e8
Can <rovahelsing@gmail.com>
2022-09-08 06:00:00 +0300

    logo design

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import style from './style.module.scss';
+import React from "react";
+import style from "./style.module.scss";
 const Header = () => {
   return (
     <div className={style.container}>
-        <div className={style.world}>            
+      <div className={style.leftHeader}>
+        <div className={style.worldLogo}>       
         </div>
-        <div className={style.shadow}>            
+        <div className={style.head}>
+          <h4>EXPLORE THE SOLAR SYSTEM</h4>
         </div>
+      </div>
+      <div className={style.rightHeader}></div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
