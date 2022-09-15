b0cc1d555bfc50c1d05298379a4c19be164bc94d
Can <rovahelsing@gmail.com>
2022-09-16 00:42:13 +0300

    two planets were added

--- src/Components/Planets/PlanetCreator.js
+++ src/Components/Planets/PlanetCreator.js
@@ -1,13 +1,54 @@
-import { OrbitControls, Stars } from "@react-three/drei";
-import { useLoader } from "@react-three/fiber";
-import React from "react";
+/* eslint-disable no-undef */
+import { OrbitControls, PerspectiveCamera, Stars } from "@react-three/drei";
+import { useFrame, useLoader } from "@react-three/fiber";
+import React, { useRef } from "react";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
 import texture from "../../Assets/img/PlanetsTexture/earthmap1kcloud.png";
 const PlanetCreator = () => {
   const map = useLoader(TextureLoader, texture);
+ 
+
+//   const width = window.innerHeight;
+//   const height = window.innerHeight;
+  //   const camera = new THREE.PerspectiveCamera( // 1'den fazla kamera çeşidi var burda PerspectiveCamera ekledik
+  //     // neden ?
+  //     45, //?
+  //     window.innerWidth / window.innerHeight, //?
+  //     0.1, //?
+  //     1000 //?
+  //   );
+const earthRef = useRef();
+useFrame(({clock})=>{
+    const elapsedTime = clock.getElapsedTime();
+    earthRef.current.rotation.y = elapsedTime/6;
+});
+  /* <OrbitControls
+          enableZooms={true}
+          enablePan={false}
+          enableRotate={true}
+          autoRotate={false}
+          maxPolarAngle={2*Math.PI}
+          minPolarAngle={-2*Math.PI}
+          screenSpacePanning={true}
+        /> */
+
   return (
     <>
+      {/* <OrbitControls
+         camera={PerspectiveCamera}
+        /> */}
       <ambientLight intensity={0.5} />
+      <OrbitControls
+          enableZooms={true}
+          enablePan={false}
+          enableRotate={true}
+          autoRotate={false}
+          maxPolarAngle={2*Math.PI}
+          minPolarAngle={-2*Math.PI}
+          screenSpacePanning={true}
+        />
+      {/* <pointLight color="#fff" position={[2,0,2]} intensity={1.5}/> */}
+      {/* <PerspectiveCamera fov={45} aspect={width / height} near={1} far={1000}/> */}
       <Stars
         radius={300}
         depth={60}
@@ -15,15 +56,17 @@ const PlanetCreator = () => {
         count={2000}
         fade={true}
         factor={7}
-      />
-      <mesh>
+      >
+       
+      </Stars>
+
+      <mesh ref={earthRef}>
         <sphereGeometry args={[1, 32, 16]} />
-        <meshStandardMaterial map={map} />
-        <OrbitControls
-          enableZoom={true}
-          enablePan={false}
-          enableRotate={true}
-        />
+        <meshStandardMaterial map={map} />      
+      </mesh>
+      <mesh position={[20,0,0]}>
+        <sphereGeometry args={[1, 32, 16]} />
+        <meshStandardMaterial map={map} />      
       </mesh>
     </>
   );
--- src/Pages/Solar System/SolarSystem.js
+++ src/Pages/Solar System/SolarSystem.js
@@ -1,17 +1,18 @@
+import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import PlanetCreator from "../../Components/Planets/PlanetCreator";
 import style from "./style.module.scss";
 const SolarSystem = () => {
-  // const width=window.innerHeight;
-  // console.log()
-  // const height=window.innerHeight;
+  const width=window.innerHeight;
+  console.log()
+  const height=window.innerHeight;
   return (
-    <div className={style.container}   >
-      <Canvas className={style.Canvas}>
-        <Suspense fallback={null}>
-          <PlanetCreator />
-        </Suspense>
+    <div className={style.container}>
+      <Canvas className={style.Canvas} camera={{fov:95 ,aspect:(width / height), near:1 ,far:1000, position: [-10, 45, 20]}}>
+          <Suspense fallback={null}>
+            <PlanetCreator />    
+          </Suspense> 
       </Canvas>
     </div>
   );
