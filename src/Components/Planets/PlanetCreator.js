7cfd9bf3864df4b4f42bf6f32c72f71f8c9d8a7c
Can <rovahelsing@gmail.com>
2022-09-17 10:22:46 +0300

    quaternion and targeting planet

--- src/Components/Planets/PlanetCreator.js
+++ src/Components/Planets/PlanetCreator.js
@@ -1,53 +1,44 @@
 /* eslint-disable no-undef */
-import { OrbitControls, PerspectiveCamera, Stars } from "@react-three/drei";
+import { OrbitControls, Stars, Text } from "@react-three/drei";
 import { useFrame, useLoader } from "@react-three/fiber";
 import React, { useRef } from "react";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import texture from "../../Assets/img/PlanetsTexture/earthmap1kcloud.png";
+import earthTexture from "../../Assets/img/PlanetsTexture/earthmap1kcloud.png";
+import saturnTexture from "../../Assets/img/PlanetsTexture/saturnmap.jpg";
+import saturnRingTexture from "../../Assets/img/PlanetsTexture/saturnRing.png";
+import sunTexture from "../../Assets/img/PlanetsTexture/sunn.png";
+
 import * as THREE from "three";
-import { Object3D } from "three";
+import { MeshBasicMaterial } from "three";
+
 const PlanetCreator = () => {
-  const map = useLoader(TextureLoader, texture);
-  //   const renderer = new THREE.WebGLRenderer(); //?
-
-  // //   renderer.setSize(window.innerWidth, window.innerHeight); // renderlanacak sayfanın ekran boyutunu ayarlıyoruz
-  // //   // kullanıcının ekranının(window) boyutlarını girdik
-
-  //   document.body.appendChild(renderer.domElement);
-
-  // //   const width = window.innerHeight;
-  // //   const height = window.innerHeight;
-  //     const camera = new THREE.PerspectiveCamera( // 1'den fazla kamera çeşidi var burda PerspectiveCamera ekledik
-  //       // neden ?
-  //       45, //?
-  //       window.innerWidth / window.innerHeight, //?
-  //       0.1, //?
-  //       1000 //?
-  //     );
-
+  const earthMap = useLoader(TextureLoader, earthTexture);
+  const saturnMap = useLoader(TextureLoader, saturnTexture);
+  const saturnRing = useLoader(TextureLoader, saturnRingTexture);
+  const sunMap = useLoader(TextureLoader, sunTexture);
+  const text = "SATURN";
   const earthOrbitRef = useRef();
   const earthRef = useRef();
+  const ringRef = useRef();
   const sunhRef = useRef();
-  
-  // const Ref = UseUpdate(group => {
-  //     Group.rotateX(props.angle)
-  //   }, [])
-  useFrame(({ clock }) => {
+  const textRef = useRef();
+  const targetRef = useRef();
+  const obliquity = 37;
+  useFrame(({ clock, camera }) => {
     const elapsedTime = clock.getElapsedTime();
     earthRef.current.rotation.y = elapsedTime / 6;
-    earthOrbitRef.current.rotation.y += 0.001;
-    // earthRef.current.position.z= elapsedTime;
-
-    sunhRef.current.rotation.y += 0.001;
+    // earthOrbitRef.current.rotation.y += 0.001;
+    // targetRef.current.position.copy(camera.rotation);
+    targetRef.current.quaternion.copy(camera.quaternion);
+    // textRef.current.position.copy(camera.rotation);
+    // textRef.current.quaternion.copy(camera.quaternion);
+    // textRef.current.translateX(20);
+    // textRef.current.translateY(5);
+    // sunhRef.current.rotation.y += 0.001;
   });
-  /*  */
 
   return (
     <>
-      {/* <OrbitControls
-         camera={PerspectiveCamera}
-        /> */}
-
       <OrbitControls
         enableZooms={true}
         enablePan={true}
@@ -57,29 +48,64 @@ const PlanetCreator = () => {
         minPolarAngle={-2 * Math.PI}
         screenSpacePanning={false}
       />
-      {/* <pointLight color="#fff" position={[2,0,2]} intensity={1.5}/> */}
-      {/* <PerspectiveCamera fov={45} aspect={width / height} near={1} far={1000}/> */}
       <Stars
-        radius={300}
+        radius={400}
         depth={60}
         saturation={0}
-        count={1500}
-        fade={true}
+        count={2000}
+        fade={false}
         factor={7}
       ></Stars>
 
-      <mesh ref={sunhRef}>
-        <sphereGeometry args={[1, 32, 16]} />
-        <meshBasicMaterial map={map} />
-        <PerspectiveCamera />
+      <mesh ref={sunhRef} rotation={[0, 0, obliquity]}>
+        <sphereGeometry args={[5, 32, 16]} />
+        <meshBasicMaterial map={sunMap} />
         <ambientLight intensity={0} position={[0, 0, 0]} />
-        <pointLight position={[0, 0, 0]} intensity={2} distance={500} />
+        <pointLight position={[0, 0, 0]} intensity={0.5} distance={500} />
+        <Text
+          position={[5, 0, 0]}
+          fontSize={1.75}
+          letterSpacing={-0.05}
+          color="white"
+        >
+          {text}
+        </Text>
       </mesh>
-      <mesh ref={earthOrbitRef} >    
+      {/* <mesh ref={earthOrbitRef} >    
         <mesh ref={earthRef} position={[20, 0, 0]}>
           <sphereGeometry args={[1, 32, 16]} />
-          <meshStandardMaterial map={map} />
+          <meshStandardMaterial map={saturnMap} />
           <PerspectiveCamera />
+        </mesh>
+      </mesh> */}
+      <mesh castShadow receiveShadow ref={earthOrbitRef}>
+        <mesh ref={earthRef} position={[20, 0, 0]}>
+          <sphereGeometry args={[1, 32, 16]} />
+          <meshStandardMaterial map={saturnMap} />
+        </mesh>
+        <mesh
+          ref={ringRef}
+          position={[20, 0, 0]}
+          rotation-y={Math.PI * 0.2}
+          rotation-x={Math.PI * 0.8}
+        >
+          <ringGeometry args={[1, 2, 55]} />
+          <meshStandardMaterial map={saturnRing} side={THREE.DoubleSide} />
+        </mesh>
+        <mesh ref={targetRef}  position={[20, 0, 0]} >
+          <ringGeometry args={[2.4, 2.5, 32]}></ringGeometry>
+          <meshBasicMaterial color="#fff"></meshBasicMaterial>
+          <mesh ref={textRef} position={[20, 0, 0]}>
+          <Text
+            position={[-20, 5, 0]}           
+            fontSize={1.75}
+            letterSpacing={-0.05}
+            color="white"
+      
+          >
+            {text}
+          </Text>
+          </mesh>
         </mesh>
       </mesh>
     </>
--- src/Pages/Solar System/SolarSystem.js
+++ src/Pages/Solar System/SolarSystem.js
@@ -1,16 +1,24 @@
-import { OrbitControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import {  OrbitControls } from "@react-three/drei";
+import { Canvas, useLoader } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import PlanetCreator from "../../Components/Planets/PlanetCreator";
 import style from "./style.module.scss";
+// import texture from "../../Assets/img/stars.jpg";
+// import { TextureLoader } from "three/src/loaders/TextureLoader";
+import Background from "../../Components/Background/Background";
+
 const SolarSystem = () => {
+  // const map = useLoader(TextureLoader, texture);
   const width=window.innerHeight;
   console.log()
   const height=window.innerHeight;
   return (
-    <div className={style.container}>
-      <Canvas className={style.Canvas} camera={{fov:45 ,aspect:(width / height), near:1 ,far:1000, position: [-10, 45, 20]}}>
+    <div className={style.container} style={{backgroundImage:'../../Assets/img/stars.jpg'}}>
+      <Canvas className={style.Canvas}  camera={{fov:45 ,aspect:(width / height), near:1 ,far:1000, position: [-10, 45, 20]}}>
+      <primitive  object={Background} />
+      <axesHelper args={[8, 8, 8]} />
           <Suspense fallback={null}>
+        
             <PlanetCreator />    
           </Suspense> 
       </Canvas>
