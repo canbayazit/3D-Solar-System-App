cca3ce2d941915daf0a550bf943276b834829ac1
Can <rovahelsing@gmail.com>
2022-09-19 14:24:32 +0300

    rotation target according to camera

--- src/Components/Planets/PlanetCreator.js
+++ src/Components/Planets/PlanetCreator.js
@@ -7,9 +7,7 @@ import earthTexture from "../../Assets/img/PlanetsTexture/earthmap1kcloud.png";
 import saturnTexture from "../../Assets/img/PlanetsTexture/saturnmap.jpg";
 import saturnRingTexture from "../../Assets/img/PlanetsTexture/saturnRing.png";
 import sunTexture from "../../Assets/img/PlanetsTexture/sunn.png";
-
 import * as THREE from "three";
-import { MeshBasicMaterial } from "three";
 
 const PlanetCreator = () => {
   const earthMap = useLoader(TextureLoader, earthTexture);
@@ -23,14 +21,69 @@ const PlanetCreator = () => {
   const sunhRef = useRef();
   const textRef = useRef();
   const targetRef = useRef();
+  const opacityRef = useRef();
+  const merkurRef = useRef();
+  const targetOrbitRef = useRef();
+  const denemeRef = useRef();
+  const abcRef = useRef();
+  const deneme2Ref = useRef();
   const obliquity = 37;
-  useFrame(({ clock, camera }) => {
+
+  const distance = 150;
+
+  let opacityValue = 0;
+  useFrame(({ clock, camera, OrbitControls }) => {
     const elapsedTime = clock.getElapsedTime();
     earthRef.current.rotation.y = elapsedTime / 6;
-    // earthOrbitRef.current.rotation.y += 0.001;
-    // targetRef.current.position.copy(camera.rotation);
-    targetRef.current.quaternion.copy(camera.quaternion);
-    // textRef.current.position.copy(camera.rotation);
+
+    earthOrbitRef.current.rotation.y += 0.001;
+    targetOrbitRef.current.rotation.y += 0.001;
+    targetRef.current.rotation.y -= 0.001;
+    // targetRef.current.rotation.z = camera.quaternion._z;
+    // targetRef.current.rotation.x = camera.quaternion._x;
+
+    // targetRef.current.rotation.y+=0.1
+    // merkurRef.current.rotation.y += 0.0005;
+    // targetRef.current.rotation.y -= 0.001;
+    // targetRef.current.position.setFromMatrixPosition( targetRef.matrixWorld )
+    // targetRef.current.position.copy(camera.position);
+    // textRef.current.quaternion.copy(camera.quaternion);
+    // textRef.current.rotation.copy(camera.rotation);
+    deneme2Ref.current.quaternion.copy(camera.quaternion);
+    deneme2Ref.current.rotation.copy(camera.rotation);
+    // targetRef.current.quaternion.copy(camera.quaternion);
+    // targetRef.current.rotation.copy(camera.rotation);
+    // console.log("target",targetRef.current.quaternion);
+    // console.log("camera",camera.quaternion);
+    // console.log("target",abcRef.current.rotation);
+    // console.log("camera",camera.rotation);
+    // targetRef.current.rotation.x=0;
+    // targetRef.current.rotation.y=0;
+    // targetRef.current.rotation.z=0;
+    // targetRef.current.quaternion._x.copy(camera.quaternion._x);
+
+    // opacityRef.current.opacity = Math.abs(camera.position.x) / 100 + 0.1;
+    // opacityRef.current.opacity = Math.abs(camera.position.y) / 100 + 0.1;
+    // opacityRef.current.opacity = Math.abs(camera.position.z) / 100 + 0.1;
+    // opacityRef.current.opacity <= 0.60 ? opacityRef.current.opacity=0 :  opacityRef.current.opacity = Math.abs(camera.position.x) / 100 + 0.1;
+    let newDistance = Math.sqrt(
+      Math.pow(camera.position.x - earthRef.current.position.x, 2) +
+        Math.pow(camera.position.y - earthRef.current.position.y, 2) +
+        Math.pow(camera.position.z - earthRef.current.position.z, 2)
+    );
+    distance > newDistance
+      ? (opacityRef.current.opacity = 0)
+      : (opacityRef.current.opacity += newDistance);
+    // console.log(newDistance);
+    // x >= camera.quaternion._x ? opacityValue + 0.2 : opacityValue - 0.2;
+
+    // console.log("quaternion",camera.quaternion._x);
+    // console.log("opacity", opacityRef.current.opacity);
+    // console.log("position",camera.position.x);
+
+    // textRef.current.position.copy(camera.position);
+    // textRef.current.rotation.copy(camera.rotation);
+
     // textRef.current.quaternion.copy(camera.quaternion);
     // textRef.current.translateX(20);
     // textRef.current.translateY(5);
@@ -44,24 +97,22 @@ const PlanetCreator = () => {
         enablePan={true}
         enableRotate={true}
         autoRotate={false}
-        maxPolarAngle={Math.PI * 2}
-        minPolarAngle={-2 * Math.PI}
         screenSpacePanning={false}
       />
       <Stars
-        radius={400}
-        depth={60}
-        saturation={0}
-        count={2000}
+        radius={500}
+        depth={500}
+        saturation={100}
+        count={12000}
         fade={false}
-        factor={7}
+        factor={2}
       ></Stars>
 
       <mesh ref={sunhRef} rotation={[0, 0, obliquity]}>
-        <sphereGeometry args={[5, 32, 16]} />
+        <sphereGeometry args={[16, 32, 16]} />
         <meshBasicMaterial map={sunMap} />
         <ambientLight intensity={0} position={[0, 0, 0]} />
-        <pointLight position={[0, 0, 0]} intensity={0.5} distance={500} />
+        <pointLight position={[0, 0, 0]} intensity={0.5} distance={1000} />
         <Text
           position={[5, 0, 0]}
           fontSize={1.75}
@@ -78,36 +129,113 @@ const PlanetCreator = () => {
           <PerspectiveCamera />
         </mesh>
       </mesh> */}
-      <mesh castShadow receiveShadow ref={earthOrbitRef}>
-        <mesh ref={earthRef} position={[20, 0, 0]}>
-          <sphereGeometry args={[1, 32, 16]} />
+      <group castShadow receiveShadow ref={earthOrbitRef}>
+        <mesh ref={earthRef} position={[100, 0, 0]}>
+          <sphereGeometry args={[5, 32, 16]} />
           <meshStandardMaterial map={saturnMap} />
         </mesh>
         <mesh
           ref={ringRef}
-          position={[20, 0, 0]}
+          position={[100, 0, 0]}
+          rotation-y={Math.PI * 0.2}
+          rotation-x={Math.PI * 0.8}
+        >
+          <ringGeometry args={[5, 10, 55]} />
+          <meshStandardMaterial map={saturnRing} side={THREE.DoubleSide} />
+        </mesh>
+        {/* <mesh ref={targetRef} position={[100, 0, 0]}>
+          <ringGeometry args={[8.2, 9.5, 55]}></ringGeometry>
+          <meshBasicMaterial
+            ref={opacityRef}
+            color="#fff"
+            opacity={1}
+            transparent
+            side={THREE.DoubleSide}
+          ></meshBasicMaterial>
+          <mesh position={[20, 0, 0]}>
+            <Text
+              position={[-20, 8, 0]}
+              fontSize={5.75}
+              letterSpacing={-0.05}
+              color="white"
+              ref={textRef}
+            >
+              {text}
+            </Text>
+          </mesh>
+        </mesh> */}
+
+        <mesh ref={merkurRef} position={[200, 0, 0]}>
+          <sphereGeometry args={[1, 32, 16]} />
+          <meshStandardMaterial map={saturnMap} />
+          <OrbitControls
+            enableZooms={true}
+            enablePan={true}
+            enableRotate={true}
+            autoRotate={false}
+            maxPolarAngle={Math.PI * 2}
+            minPolarAngle={-2 * Math.PI}
+            screenSpacePanning={false}
+          />
+        </mesh>
+        <mesh
+          ref={ringRef}
+          position={[200, 0, 0]}
           rotation-y={Math.PI * 0.2}
           rotation-x={Math.PI * 0.8}
         >
           <ringGeometry args={[1, 2, 55]} />
           <meshStandardMaterial map={saturnRing} side={THREE.DoubleSide} />
         </mesh>
-        <mesh ref={targetRef}  position={[20, 0, 0]} >
-          <ringGeometry args={[2.4, 2.5, 32]}></ringGeometry>
-          <meshBasicMaterial color="#fff"></meshBasicMaterial>
-          <mesh ref={textRef} position={[20, 0, 0]}>
-          <Text
-            position={[-20, 5, 0]}           
-            fontSize={1.75}
-            letterSpacing={-0.05}
-            color="white"
-      
-          >
-            {text}
-          </Text>
+        <mesh position={[200, 0, 0]}>
+          <ringGeometry args={[5.2, 7.5, 55]}></ringGeometry>
+          <meshBasicMaterial
+            ref={opacityRef}
+            color="#fff"
+            opacity={0}
+            transparent
+            side={THREE.DoubleSide}
+          ></meshBasicMaterial>
+          <mesh position={[20, 0, 0]}>
+            <Text
+              position={[-20, 8, 0]}
+              fontSize={2.75}
+              letterSpacing={-0.05}
+              color="white"
+              ref={textRef}
+            >
+              {text}
+            </Text>
           </mesh>
         </mesh>
-      </mesh>
+      </group>
+      <group ref={targetOrbitRef}>
+        <mesh position={[100, 0, 0]} ref={targetRef}>
+          <mesh ref={deneme2Ref}>
+          <mesh ref={denemeRef}>
+            <ringGeometry  args={[13.2, 14.5, 55]}></ringGeometry>
+            <meshBasicMaterial
+              ref={opacityRef}
+              color="#fff"
+              opacity={1}
+              transparent
+              side={THREE.DoubleSide}
+            ></meshBasicMaterial>
+          </mesh>
+          <mesh position={[20, 14, 0]}>
+            <Text
+              position={[-20, 8, 0]}
+              fontSize={8.75}
+              letterSpacing={-0.05}
+              color="white"
+              ref={textRef}
+            >
+              {text}
+            </Text>
+          </mesh>
+          </mesh>
+        </mesh>
+      </group>
     </>
   );
 };
--- src/Pages/Solar System/SolarSystem.js
+++ src/Pages/Solar System/SolarSystem.js
@@ -1,4 +1,4 @@
-import {  OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 import { Canvas, useLoader } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import PlanetCreator from "../../Components/Planets/PlanetCreator";
@@ -9,18 +9,25 @@ import Background from "../../Components/Background/Background";
 
 const SolarSystem = () => {
   // const map = useLoader(TextureLoader, texture);
-  const width=window.innerHeight;
-  console.log()
-  const height=window.innerHeight;
+  const width = window.innerHeight;
+  console.log();
+  const height = window.innerHeight;
   return (
-    <div className={style.container} style={{backgroundImage:'../../Assets/img/stars.jpg'}}>
-      <Canvas className={style.Canvas}  camera={{fov:45 ,aspect:(width / height), near:1 ,far:1000, position: [-10, 45, 20]}}>
-      <primitive  object={Background} />
-      <axesHelper args={[8, 8, 8]} />
-          <Suspense fallback={null}>
-        
-            <PlanetCreator />    
-          </Suspense> 
+    <div className={style.container}>
+      <Canvas
+        className={style.Canvas}
+        camera={{
+          fov: 45,
+          aspect: width / height,
+          near: 1,
+          far: 4000,
+          position: [-10, 45, 20],
+        }}
+      >
+        <axesHelper args={[25, 25, 25]} />
+        <Suspense fallback={null}>
+          <PlanetCreator />
+        </Suspense>
       </Canvas>
     </div>
   );
